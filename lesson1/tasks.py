60399197246fa7f02377437323972a6660bcedc3
Anton Chepelev <chepelash@gmail.com>
2022-07-08 23:03:54 +0300

    Completed task3

--- lesson1/tasks.py
+++ lesson1/tasks.py
@@ -37,8 +37,41 @@ def task2():
         print("You did not")
     
 
+def task3():
+    """
+    3. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+    в которой находится эта точка (или на какой оси она находится).
+    *Пример:*
+    - x=34; y=-30 -> 4
+    - x=2; y=4-> 1
+    - x=-34; y=-30 -> 3
+    """
+    input_string = input("Enter x and y  separated by a space > ")    
+    try:
+        x, y = map(int, input_string.split(" "))
+    except ValueError:
+        print("Wrong input")
+        return
+    
+    if x == 0 or y == 0:
+        print("x and y must not be zero")
+        return
+    
+    print(f"x = {x}; y = {y} -> ", end="")
+    if y > 0:
+        if x > 0:
+            print(1)
+        else:
+            print(2)
+    else:
+        if x > 0:
+            print(4)
+        else:
+            print(3)
+
+
 def main():
-    task_list = [task1, task2]
+    task_list = [task1, task2, task3]
     for task in task_list:
         print(f"{task.__name__}...")    
         task()
