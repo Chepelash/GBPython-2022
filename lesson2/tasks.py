9b7998405b6e8c20f6a67d55a368e526d53ab497
Anton Chepelev <chepelash@gmail.com>
2022-07-11 22:59:43 +0300

    task1 completed

--- lesson2/tasks.py
+++ lesson2/tasks.py
@@ -1,8 +1,32 @@
 def task1():
-    pass
+    """
+    Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+    Пример:
+    - 6782 -> 23
+    - 0,56 -> 11
+    """    
+    input_number_string = input("Enter a real number > ")
+    input_number_string = input_number_string.replace(",", ".")
+    try:
+        float(input_number_string)
+    except ValueError:
+        print("Wrong input")
+        return
+    
+    sum = 0
+    for symb in input_number_string:
+        if symb == '.':
+            continue
+        sum += int(symb)
+    print(f"- {input_number_string} -> {sum}")
 
 
 def task2():
+    """
+    Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+    Пример:
+    - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+    """
     pass
 
 
