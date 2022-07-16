e26d7062beb2df13e6e63e440cbb3a67c64729cf
Anton Chepelev <chepelash@gmail.com>
2022-07-16 10:50:51 +0300

    task2. Added list shuffle. task3 done

--- lesson3/tasks.py
+++ lesson3/tasks.py
@@ -26,7 +26,8 @@ def task2():
     """
     my_list = [''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 5))) for _ in range(random.randint(0, 7))]
     if random.randint(0, 5) and len(my_list) != 0:
-        my_list.append(my_list[random.randrange(0, len(my_list))])        
+        my_list.append(my_list[random.randrange(0, len(my_list))])
+    random.shuffle(my_list)
     print(my_list)
     
     string_to_find = input("Enter a string to find second entrance > ")
@@ -43,7 +44,15 @@ def task3():
     и выводить для каждого уникального слова в этой строке число его повторений (без учёта регистра) 
     в формате "слово количество" (см. пример вывода).
     """
-    pass
+    big_string = input("Enter your esse > ")
+    my_dict = dict()
+    for word in map(str.lower, big_string.split(" ")):
+        if word in my_dict:
+            my_dict[word] += 1
+        else:
+            my_dict[word] = 1
+    for k, v in my_dict.items():
+        print(f"{k} {v}")
 
 
 def main():
