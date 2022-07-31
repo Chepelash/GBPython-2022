07dc87ef287f67ad29a87dcfa7ead64939cbaaf3
Anton Chepelev <chepelash@gmail.com>
2022-07-31 22:57:18 +0300

    Adder complex operation function

--- lesson7/controller/controller.py
+++ lesson7/controller/controller.py
@@ -2,3 +2,6 @@ from model import *
 
 def rational_operation(expression_to_solve: str):
     return rational.evaluate(expression_to_solve)
+
+def complex_operation(a: str, b: str, operation: str):
+    return complex.evaluate(a, b, operation)
