0b4bd4d5f22ba930c7f71c58af56400f684cd1d7
Anton Chepelev <chepelash@gmail.com>
2022-08-02 12:01:00 +0300

    Added complex interface

--- lesson7/view/interface.py
+++ lesson7/view/interface.py
@@ -9,12 +9,14 @@ q - выйти из программы
 числа - разные режимы работы"""
     user_input = ""
     print(HELP_TEXT)
+    print()
     while user_input.lower() != 'q':
         user_input = input("""Введите 1 для работы с рациональными числами
 Введите 2 для работы с комплексными числами 
 h - для вывода помощи
 q - для выхода > """)
         if user_input == '1':
+            logger.log_user_request(__name__, "Rational numbers operation")
             user_input = input("Наберите выражение > ")
             logger.log_user_request(__name__, user_input)
             try:
@@ -37,17 +39,31 @@ q - для выхода > """)
 2 - разность
 3 - умножение
 4 - деление""")
+            logger.log_user_request(__name__, "Complex numbers operation")
+
             a_number = input("Введите первое комплексное число > ").replace(" ", "")
+            logger.log_user_request(__name__, f"a_number = {a_number}")
             b_number = input("Введите второе комплексное число > ").replace(" ", "")
-            operation = input("Введите операцию > ")
+            logger.log_user_request(__name__, f"b_number = {b_number}")
+            operation = input("Введите номер операции > ")
+            logger.log_user_request(__name__, f"operation = {operation}")
+
             try:
-                controller.complex_operation(a_number, b_number, operation)
-            except Exception as e:
-                print("TODO Here")
+                print(f"Результат = {controller.complex_operation(a_number, b_number, operation)}")
+            except ValueError as e:
+                logger.log_error(e.args[0], e.args[1])
+                print("Ошибка во вводе")
+            except NotImplementedError as e:
+                logger.log_error(e.args[0], e.args[1])
+                print("Операция не существует")
+            except Exception as e:                
+                logger.log_error(e.args[0], e.args[1])
+                print("Неизвестная ошибка")
         elif user_input.lower() == 'q':
             print("Выход из программы")
+            logger.log_user_request(__name__, "Quitting")
             continue
         elif user_input.lower() == 'h':
             print(HELP_TEXT)
         else:
-            print("Неизвестная команда")            
+            print("Неизвестная команда")
