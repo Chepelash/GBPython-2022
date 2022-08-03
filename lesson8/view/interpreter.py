7a1a265314eaba414e7b01a68384ba6b52476edf
Anton Chepelev <chepelash@gmail.com>
2022-08-03 12:27:12 +0300

    Added print data for testing

--- lesson8/view/interpreter.py
+++ lesson8/view/interpreter.py
@@ -1,28 +1,32 @@
 from logger import logger
+from types import FunctionType
 
 
-def show_dict_data(controller_function: function):
+def show_dict_data(controller_function: FunctionType):
     try:
         data = controller_function()
     except NotImplementedError as e:
         logger.log_error(e.args[0], e.args[1])
         print("Не сделано")
         return
+    print(data)
 
 
-def import_table(controller_function: function):
+def import_table(controller_function: FunctionType):
     try:
         data = controller_function()
     except NotImplementedError as e:
         logger.log_error(e.args[0], e.args[1])
         print("Не сделано")
         return
+    print(data)
 
 
-def export_table(controller_function: function):
+def export_table(controller_function: FunctionType):
     try:
         data = controller_function()
     except NotImplementedError as e:
         logger.log_error(e.args[0], e.args[1])
         print("Не сделано")
         return
+    print(data)
