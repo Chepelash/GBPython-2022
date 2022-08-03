0aaec603e2b7e03777fc4a3e064e00e231a05dab
Anton Chepelev <chepelash@gmail.com>
2022-08-03 12:27:44 +0300

    added show_all_workers method

--- lesson8/model/manipulator.py
+++ lesson8/model/manipulator.py
@@ -1,4 +1,6 @@
 import os
+import csv
+from collections import OrderedDict
 
 
 ASSIGNMENT_FILE_PATH = os.path.join("config", 'assignments.csv')
@@ -12,8 +14,35 @@ def is_file_exists(fpath: str) -> bool:
     return os.path.isfile(fpath)
 
 
-def show_all_workers():
-    raise NotImplementedError(__name__, "Not implemented")
+def show_all_workers() -> dict:
+    if not os.path.isfile(WORKERS_FILE_PATH) or not os.path.isfile(JOBS_FILE_PATH) \
+    or not os.path.isfile(DEPARTMENT_FILE_PATH) or not os.path.isfile(ASSIGNMENT_FILE_PATH):
+        print(os.path.abspath('.'))
+        raise FileNotFoundError(__name__, "BD files not found")
+    result_dict = {}
+    with open(WORKERS_FILE_PATH, 'r') as f:
+        csv_reader = csv.DictReader(f, delimiter=";")
+        for line in csv_reader:
+            result_dict[line["id"]] = OrderedDict({"id": line["id"], "fio": line["fio"], "phone": line["phone"]})
+    with open(ASSIGNMENT_FILE_PATH, 'r') as ass_file, open(JOBS_FILE_PATH, 'r') as jobs_file, open(DEPARTMENT_FILE_PATH, 'r') as dep_file:
+        ass_reader = csv.DictReader(ass_file, delimiter=";")
+        jobs_reader = csv.DictReader(jobs_file, delimiter=";")
+        dep_reader = csv.DictReader(dep_file, delimiter=";")
+        for line in ass_reader:
+            id_worker = line["id_worker"]
+            id_job = line["id_job"]
+            id_department = line["id_department"]
+            jobs_file.seek(0)
+            dep_file.seek(0)
+            for line in jobs_reader:
+                if line["id"] == id_job:
+                    result_dict[id_worker]["job"] = line["job"]                   
+                    break
+            for line in dep_reader:
+                if line["id"] == id_department:
+                    result_dict[id_worker]["department"] = line["department"]
+                    break
+    return result_dict    
 
 
 def add_new_worker(data: dict):
