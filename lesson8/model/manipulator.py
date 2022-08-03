9757383bdb702fbdc23a38571e655e752c6b4797
Anton Chepelev <chepelash@gmail.com>
2022-08-03 12:44:30 +0300

    Added show all method to everything

--- lesson8/model/manipulator.py
+++ lesson8/model/manipulator.py
@@ -8,39 +8,43 @@ DEPARTMENT_FILE_PATH = os.path.join("config", 'departments.csv')
 JOBS_FILE_PATH = os.path.join("config", 'jobs.csv')
 WORKERS_FILE_PATH = os.path.join("config", 'workers.csv')
 
-
-def is_file_exists(fpath: str) -> bool:
-    """checks if file still there"""
-    return os.path.isfile(fpath)
+CSV_DELIMITERS = ";"
+ID_FIELD = "id"
+FIO_FIELD = "fio"
+PHONE_FIELD = "phone"
+DEPARTMENT_FIELD = "department"
+JOB_FIELD = "job"
+ID_WORKER_FIELD = "id_worker"
+ID_JOB_FIELD = "id_job"
+ID_DEPARTMENT_FIELD = "id_department"
 
 
 def show_all_workers() -> dict:
     if not os.path.isfile(WORKERS_FILE_PATH) or not os.path.isfile(JOBS_FILE_PATH) \
     or not os.path.isfile(DEPARTMENT_FILE_PATH) or not os.path.isfile(ASSIGNMENT_FILE_PATH):
-        print(os.path.abspath('.'))
         raise FileNotFoundError(__name__, "BD files not found")
     result_dict = {}
     with open(WORKERS_FILE_PATH, 'r') as f:
-        csv_reader = csv.DictReader(f, delimiter=";")
+        csv_reader = csv.DictReader(f, delimiter=CSV_DELIMITERS)
         for line in csv_reader:
-            result_dict[line["id"]] = OrderedDict({"id": line["id"], "fio": line["fio"], "phone": line["phone"]})
+            result_dict[line[ID_FIELD]] = OrderedDict({ID_FIELD: line[ID_FIELD], FIO_FIELD: line[FIO_FIELD], PHONE_FIELD: line[PHONE_FIELD]})
     with open(ASSIGNMENT_FILE_PATH, 'r') as ass_file, open(JOBS_FILE_PATH, 'r') as jobs_file, open(DEPARTMENT_FILE_PATH, 'r') as dep_file:
-        ass_reader = csv.DictReader(ass_file, delimiter=";")
-        jobs_reader = csv.DictReader(jobs_file, delimiter=";")
-        dep_reader = csv.DictReader(dep_file, delimiter=";")
+        ass_reader = csv.DictReader(ass_file, delimiter=CSV_DELIMITERS)
+        jobs_reader = csv.DictReader(jobs_file, delimiter=CSV_DELIMITERS)
+        dep_reader = csv.DictReader(dep_file, delimiter=CSV_DELIMITERS)
         for line in ass_reader:
-            id_worker = line["id_worker"]
-            id_job = line["id_job"]
-            id_department = line["id_department"]
+            id_worker = line[ID_WORKER_FIELD]
+            id_job = line[ID_JOB_FIELD]
+            id_department = line[ID_DEPARTMENT_FIELD]
             jobs_file.seek(0)
             dep_file.seek(0)
             for line in jobs_reader:
-                if line["id"] == id_job:
-                    result_dict[id_worker]["job"] = line["job"]                   
+                if line[ID_FIELD] == id_job:
+                    result_dict[id_worker][JOB_FIELD] = line[JOB_FIELD]                   
                     break
             for line in dep_reader:
-                if line["id"] == id_department:
-                    result_dict[id_worker]["department"] = line["department"]
+                if line[ID_FIELD] == id_department:
+                    result_dict[id_worker][DEPARTMENT_FIELD] = line[DEPARTMENT_FIELD]
                     break
     return result_dict    
 
@@ -57,8 +61,15 @@ def edit_worker(data: dict):
     raise NotImplementedError(__name__, "Not implemented")
 
 
-def show_all_departments():
-    raise NotImplementedError(__name__, "Not implemented")
+def show_all_departments() -> dict:
+    if not os.path.isfile(DEPARTMENT_FILE_PATH):
+        raise FileNotFoundError(__name__, "BD files not found")
+    result_dict = {}
+    with open(DEPARTMENT_FILE_PATH, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=CSV_DELIMITERS)
+        for line in csv_reader:
+            result_dict[line[ID_FIELD]] = OrderedDict({ID_FIELD: line[ID_FIELD], DEPARTMENT_FIELD: line[DEPARTMENT_FIELD]})
+    return result_dict
 
 
 def add_new_department(data: dict):
@@ -74,7 +85,14 @@ def edit_department(data: dict):
 
 
 def show_all_jobs():
-    raise NotImplementedError(__name__, "Not implemented")
+    if not os.path.isfile(JOBS_FILE_PATH):
+        raise FileNotFoundError(__name__, "BD files not found")
+    result_dict = {}
+    with open(JOBS_FILE_PATH, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=CSV_DELIMITERS)
+        for line in csv_reader:
+            result_dict[line[ID_FIELD]] = OrderedDict({ID_FIELD: line[ID_FIELD], JOB_FIELD: line[JOB_FIELD]})
+    return result_dict
 
 
 def add_new_job(data: dict):
