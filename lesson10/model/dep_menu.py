67c39f3720c3376df6300f6bc423793092c3823d
Anton Chepelev <chepelash@gmail.com>
2022-08-16 10:59:04 +0300

    Copied from worker menu
    I guess class with slight changes would be better for the sameish menues

---
+++ lesson10/model/dep_menu.py
@@ -0,0 +1,83 @@
+
+from telegram import (
+    Update,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,      
+)
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    ConversationHandler,
+    CallbackQueryHandler,
+)
+
+import state_constants as sc
+import manipulator
+#states
+(
+    START_MENU, 
+    SHOW_DATA,
+    SELECT_ACTION,
+    RETURN_TO_MAIN, 
+    BACK
+) = map(lambda x: __name__ + x, map(str, range(5)))
+
+# functions
+def show_all_departments(update: Update, context: CallbackContext):
+    try:
+        data = manipulator.show_all_departments()
+    except Exception:
+        text = "Error =("
+    else:
+        text = ""
+        for v in data.values():
+            text += " ".join(list(v.values())) + '\n'
+        
+    buttons = [[InlineKeyboardButton("Назад", callback_data=BACK)]]
+    keyboard = InlineKeyboardMarkup(buttons)
+    update.callback_query.answer()    
+    update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
+    return SHOW_DATA
+
+def start_menu(update: Update, context: CallbackContext):
+    text = "Меню департаментов"
+    buttons = [
+        [InlineKeyboardButton("Показать всех", callback_data=SHOW_DATA)],
+        [InlineKeyboardButton("В главное меню", callback_data=RETURN_TO_MAIN)]
+    ]
+    keyboard = InlineKeyboardMarkup(buttons)
+    update.callback_query.answer()    
+    update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
+    return SELECT_ACTION
+
+def return_upper(update: Update, context: CallbackContext):
+    context.user_data[sc.START_OVER] = True
+    context.user_data[sc.MAIN_MENU_FUNCTION](update, context)
+    return sc.END
+
+def stop_submenu(update: Update, context: CallbackContext):
+    update.message.reply_text("Bye")
+    return sc.STOPPING
+#worker CH
+menu_handlers = [
+    CallbackQueryHandler(show_all_departments, pattern='^'+SHOW_DATA+'$'),
+    # CallbackQueryHandler(add_worker, pattern='^'+ADD_WORKER+'$'),
+    # CallbackQueryHandler(edit_worker, pattern='^'+EDIT_WORKER+'$'),
+    # CallbackQueryHandler(return_upper, pattern='^' + RETURN_TO_MAIN + '$'),
+]
+conv_handler = ConversationHandler(
+    entry_points=[CallbackQueryHandler(start_menu, pattern='^'+START_MENU+'$')],
+    states={
+        SELECT_ACTION: menu_handlers,
+        SHOW_DATA: [CallbackQueryHandler(start_menu, pattern='^'+BACK+'$')]
+    },
+    fallbacks=[ 
+        CommandHandler('stop', stop_submenu),
+        CallbackQueryHandler(return_upper, pattern='^' + RETURN_TO_MAIN + '$'),
+    ],
+    map_to_parent={
+        sc.END: sc.SELECTING_MENU,
+        sc.STOPPING: sc.STOPPING
+    }
+)
+
